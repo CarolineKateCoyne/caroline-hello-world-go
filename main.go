bc39f559129e87048eb37bd9b5668678e4a9d42d
CarolineKateCoyne <113974951+CarolineKateCoyne@users.noreply.github.com>
2024-04-23 12:19:05 +0100

    Fix code

--- main.go
+++ main.go
@@ -5,39 +5,34 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 	"fmt"
-	"net/http"
 )
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-    
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "OK")
+
+func healthCheckHandler(c *fiber.Ctx) error {
+	return c.SendString("OK")
 }
 
 func getEnv(key, fallback string) string {
-    if value, ok := os.LookupEnv(key); ok {
-        return value
-    }
-    return fallback
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
 
 func main() {
 	engine := html.New("./views", ".html")
 
 	app := fiber.New(fiber.Config{
-        Views: engine,
-    })
+		Views: engine,
+	})
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        // Render index template
-        return c.Render("index", nil)
-    })
-	
-	app.Listen(":"+getEnv("PORT", "8080"))
-	{
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Render index template
+		return c.Render("index", nil)
+	})
 
-    http.HandleFunc("/health", healthCheckHandler)
+	app.Get("/health", healthCheckHandler)
 
-    fmt.Println("Starting server on port 8080...")
-    http.ListenAndServe(":8080", nil)
-}
+	port := getEnv("PORT", "8080")
+	fmt.Println("Starting server on port", port)
+	app.Listen(":" + port)
 }
