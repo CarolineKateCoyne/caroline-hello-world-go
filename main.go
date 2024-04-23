f764c9823571cd454d76cb0a4fb3e4d5378d361e
CarolineKateCoyne <113974951+CarolineKateCoyne@users.noreply.github.com>
2024-04-23 11:36:26 +0100

    Healthcheck

--- main.go
+++ main.go
@@ -4,7 +4,14 @@ import (
 	"os"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
+	"fmt"
+	"net/http"
 )
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+    
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "OK")
+}
 
 func getEnv(key, fallback string) string {
     if value, ok := os.LookupEnv(key); ok {
@@ -26,4 +33,11 @@ func main() {
     })
 	
 	app.Listen(":"+getEnv("PORT", "8080"))
+	{
+
+    http.HandleFunc("/health", healthCheckHandler)
+
+    fmt.Println("Starting server on port 8080...")
+    http.ListenAndServe(":8080", nil)
+}
 }
